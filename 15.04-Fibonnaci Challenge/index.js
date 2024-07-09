6e50264adeee6932938f513fcbff297276c61560
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-09 21:58:00 +0200

    final Fibonacci Challenge

--- 15.04-Fibonnaci Challenge/index.js
+++ 15.04-Fibonnaci Challenge/index.js
@@ -4,18 +4,25 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const userNumber = Number(event.target.elements.number.value);
+  const fibonacciMessage = document.createElement("h2");
 
   let fibonacci = [0, 1];
-  for (
-    let sequenceLength = fibonacci.length;
-    sequenceLength <= userNumber;
-    sequenceLength++
-  ) {
+  if (userNumber === 0) {
+    fibonacciMessage.textContent = fibonacci[0];
+  }
+  if (userNumber === 1) {
+    fibonacciMessage.textContent = fibonacci.slice(0, 2);
+  }
+  if (userNumber === 2) {
+    fibonacciMessage.textContent = fibonacci.slice(0, 2);
+  }
+  for (i = fibonacci.length; i <= userNumber; i++) {
     let newNumber =
       fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
     fibonacci.push(newNumber);
-    const fibonacciMessage = document.createElement("h2");
-    fibonacciMessage.textContent = fibonacci;
-    codeBox.append(fibonacciMessage);
   }
+
+  fibonacciMessage.textContent = fibonacci.slice(0, userNumber + 1);
+
+  codeBox.append(fibonacciMessage);
 });
