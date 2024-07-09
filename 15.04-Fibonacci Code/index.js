5818c3eba17f99f07804c56a88e551061b1fa6ce
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-09 11:36:27 +0200

    delete fibonacci folder

--- 15.04-Fibonacci Code/index.js
+++
@@ -1,8 +0,0 @@
-const fibonacci = [0];
-
-function fibonacciGenerator() {
-  for (let n = 0; 0 <= n; ) console.log(fibonacci.length);
-  console.log(fibonacci);
-}
-fibonacciGenerator();
-fibonacciGenerator();
