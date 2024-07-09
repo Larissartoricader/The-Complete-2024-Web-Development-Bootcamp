a0ada6317512e10d035a781680f9d6fc07dab7af
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-09 10:21:26 +0200

    Function of 99 Bottles Challenge with Loop

---
+++ 15.03-99 Bottles/index.js
@@ -0,0 +1,19 @@
+const bodyElement = document.querySelector("body");
+const bottles = [];
+let count = 1;
+
+function beerGame() {
+  while (count <= 100) {
+    bottles.push(101 - count);
+    console.log(bottles);
+    count++;
+    const aBottleLess = bottles[bottles.length - 1] - 1;
+    const bottlesNr = document.createElement("h2");
+    bottlesNr.textContent = `${
+      bottles[bottles.length - 1]
+    } bottles of beer on the wall, ${
+      bottles[bottles.length - 1]
+    } bottles of beer. Take one down and pass it around, ${aBottleLess} bottle of beer on the wall`;
+    bodyElement.append(bottlesNr);
+  }
+}
