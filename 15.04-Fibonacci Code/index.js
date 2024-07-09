5d5a9ddc0d91e6abb8f2297bb54b921b2d1a8fed
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-09 11:22:34 +0200

    Fibonacci start

---
+++ 15.04-Fibonacci Code/index.js
@@ -0,0 +1,10 @@
+const fibonacci = [0];
+
+function fibonacciGenerator() {
+  for (let n = 0; 0 <= n; ) console.log(fibonacci.length);
+  console.log(fibonacci);
+}
+fibonacciGenerator();
+fibonacciGenerator();
+// fibonacciGenerator();
+// fibonacciGenerator();
