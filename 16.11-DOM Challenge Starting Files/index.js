ac09cf1a76d643935e49c554bfb118171674dd4b
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-10 11:44:26 +0200

    combine selectors

--- 16.11-DOM Challenge Starting Files/index.js
+++ 16.11-DOM Challenge Starting Files/index.js
@@ -2,3 +2,7 @@ const listElement = document.querySelector("ul").lastElementChild;
 console.log(listElement);
 
 listElement.innerHTML = "Larissa";
+
+const googleListElement = document.querySelector(".list a");
+console.log(googleListElement);
+googleListElement.style.color = "red";
