452f83aa961b8e3c1e637970618a9761d8fcb32c
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-12 12:32:23 +0200

    code updated

--- 18.13-Drum-Kit-Starting-Files/index.js
+++ 18.13-Drum-Kit-Starting-Files/index.js
@@ -1,8 +1,6 @@
 const allButtons = document.querySelectorAll("button");
 
-// SOLUTION with Keypress
-document.addEventListener("keypress", function (event) {
-  let classButton = event.key;
+function makeSound(classButton) {
   switch (true) {
     case classButton.includes("w"):
       const kick = new Audio("sounds/kick-bass.mp3");
@@ -36,6 +34,11 @@ document.addEventListener("keypress", function (event) {
     default:
       console.log("Something Wrong " + classButton);
   }
+}
+// SOLUTION with Keypress
+document.addEventListener("keypress", function (event) {
+  let classButton = event.key;
+  makeSound(classButton);
 });
 
 // SOLUTION with SWITCH
@@ -43,94 +46,6 @@ document.addEventListener("keypress", function (event) {
 allButtons.forEach((button) => {
   button.addEventListener("click", () => {
     let classButton = button.className;
-    switch (true) {
-      case classButton.includes("w"):
-        const kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-      case classButton.includes("a"):
-        const crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-      case classButton.includes("s"):
-        const snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      case classButton.includes("d"):
-        const tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case classButton.includes("j"):
-        const tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case classButton.includes("k"):
-        const tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case classButton.includes("l"):
-        const tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      default:
-        console.log("Something Wrong " + classButton);
-    }
-
-    // SOLUTION WITH IF/ELSE
-
-    // let rightAudio;
-    // const buttonClass = button.className;
-
-    // if (buttonClass.includes("w")) {
-    //   rightAudio = "./sounds/kick-bass.mp3";
-    // } else if (buttonClass.includes("a")) {
-    //   rightAudio = "./sounds/crash.mp3";
-    // } else if (buttonClass.includes("s")) {
-    //   rightAudio = "./sounds/snare.mp3";
-    // } else if (buttonClass.includes("d")) {
-    //   rightAudio = "./sounds/tom-1.mp3";
-    // } else if (buttonClass.includes("j")) {
-    //   rightAudio = "./sounds/tom-2.mp3";
-    // } else if (buttonClass.includes("l")) {
-    //   rightAudio = "./sounds/tom-3.mp3";
-    // } else if (buttonClass.includes("l")) {
-    //   rightAudio = "./sounds/tom-4.mp3";
-    // }
-
-    // if (rightAudio) {
-    //   const audio = new Audio(rightAudio);
-    //   audio.play();
-    // }
+    makeSound(classButton);
   });
 });
-
-// const allButtons = document.querySelectorAll("button");
-
-// allButtons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     let rightAudio;
-//     const buttonClass = button.className;
-
-//     if (buttonClass.includes("w")) {
-//       rightAudio = "./sounds/kick-bass.mp3";
-//     } else if (buttonClass.includes("a")) {
-//       rightAudio = "./sounds/crash.mp3";
-//     } else if (buttonClass.includes("s")) {
-//       rightAudio = "./sounds/snare.mp3";
-//     } else if (buttonClass.includes("d")) {
-//       rightAudio = "./sounds/tom-1.mp3";
-//     } else if (buttonClass.includes("j")) {
-//       rightAudio = "./sounds/tom-2.mp3";
-//     } else if (buttonClass.includes("l")) {
-//       rightAudio = "./sounds/tom-3.mp3";
-//     } else if (buttonClass.includes("l")) {
-//       rightAudio = "./sounds/tom-4.mp3";
-//     }
-
-//     if (rightAudio) {
-//       const audio = new Audio(rightAudio);
-//       audio.play();
-//     }
-//   });
-// });
