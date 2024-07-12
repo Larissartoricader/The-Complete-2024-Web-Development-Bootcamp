09fee53776489af7c8c7644fff9a52626ffa854f
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-12 10:16:37 +0200

    Sounds to the Classname

--- 18.13-Drum-Kit-Starting-Files/index.js
+++ 18.13-Drum-Kit-Starting-Files/index.js
@@ -1,14 +1,29 @@
-// const buttonW = document.querySelector(".w");
-// const buttonA = document.querySelector(".a");
-// const buttonS = document.querySelector(".s");
-// const buttonD = document.querySelector(".d");
-// const buttonJ = document.querySelector(".j");
-// const buttonK = document.querySelector(".k");
-// const buttonL = document.querySelector(".l");
 const allButtons = document.querySelectorAll("button");
 
-const audio = new Audio("./sounds/tom-1.mp3");
-
 allButtons.forEach((button) => {
-  button.addEventListener("click", () => audio.play());
+  button.addEventListener("click", () => {
+    let rightAudio;
+    const buttonClass = button.className;
+
+    if (buttonClass.includes("w")) {
+      rightAudio = "./sounds/kick-bass.mp3";
+    } else if (buttonClass.includes("a")) {
+      rightAudio = "./sounds/crash.mp3";
+    } else if (buttonClass.includes("s")) {
+      rightAudio = "./sounds/snare.mp3";
+    } else if (buttonClass.includes("d")) {
+      rightAudio = "./sounds/tom-1.mp3";
+    } else if (buttonClass.includes("j")) {
+      rightAudio = "./sounds/tom-2.mp3";
+    } else if (buttonClass.includes("l")) {
+      rightAudio = "./sounds/tom-3.mp3";
+    } else if (buttonClass.includes("l")) {
+      rightAudio = "./sounds/tom-4.mp3";
+    }
+
+    if (rightAudio) {
+      const audio = new Audio(rightAudio);
+      audio.play();
+    }
+  });
 });
