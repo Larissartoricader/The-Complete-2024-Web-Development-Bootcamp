268c55dd48a3d0cfc677afd938ea6ad365d69318
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-15 11:46:04 +0200

    jQuery Tests

--- 19.15-JQuery/index.js
+++ 19.15-JQuery/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-  $("h1").addClass("new-h1").text("Good Bye");
+  $("h1").after("<h2>Dom Manipulation</h2>");
+  $("h2").after(
+    "Using $ is possible to select one or all html selector. You can use the methode .before or .after to decide where the new html selectors should be places. Be aware that jQuery also offers append and prepend. Those place the new html code inside the selected html selector, like < paragraf > < newselector >Text < paragraf >. Preppend places it at the begging the selected html and append at end of the selected html"
+  );
 
   $("body").removeClass("new-body");
 
@@ -19,10 +22,41 @@ $("h1").click(function () {
 });
 
 $("button").click(function () {
-  $("h1").css("color", "red");
+  $("h1")
+    .slideUp()
+    .slideDown()
+    .slideUp()
+    .slideDown()
+    .slideUp()
+    .slideDown()
+    .slideUp()
+    .slideDown()
+    .slideUp()
+    .slideDown()
+    .slideUp()
+    .slideDown()
+    .animate({ margin: "25%" });
+  // hide
+  // show
+  // toggle
+  // fadeIn
+  // fadeOut
+  // slideUp
+  // slideDown
+  // slideToggle
+  // animate (only number) .animate({ margin: "50%" })
 });
 
 $("input").keypress(function (event) {
   const letter = event.key;
   $("h1").text(letter);
 });
+
+$("footer").on("mouseover", function () {
+  $("body").css("background-color", "hotpink");
+});
+
+$("a").before("<h2>A New Headline from jQuery before selector a");
+$("a").after("<h2>A New Headline from jQuery after selector a");
+$("p").prepend("Text Preprend => ");
+$("p").append(" <button> <= Append </button");
