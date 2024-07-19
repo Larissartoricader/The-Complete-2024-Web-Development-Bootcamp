dfa603fabe60e8dd05f194bdac8aa14516b04329
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-19 11:39:17 +0200

    npm install and usage

--- 23.192-npm/index.js
+++ 23.192-npm/index.js
@@ -1,6 +1,12 @@
 // const generateName = require("sillyname");
-// in package.json type mode allowrs import instead require
+// in package.json type mode allows import instead require
 import generateName from "sillyname";
 const sillyName = generateName();
 
 console.log("My Name is " + sillyName);
+
+import superheros from "superheros";
+
+const name = superheros();
+
+console.log(`Here is my NEW Super Hero Name ${name}`);
