9850245563ea992cb791ff8d34b9ffd9ec10c321
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-19 10:49:47 +0200

    npm install and configuration

---
+++ 23.192-npm/index.js
@@ -0,0 +1,6 @@
+// const generateName = require("sillyname");
+// in package.json type mode allowrs import instead require
+import generateName from "sillyname";
+const sillyName = generateName();
+
+console.log("My Name is " + sillyName);
