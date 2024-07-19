7ac95b281b3b45b6332f0329c5cf2b3f9dbe2a49
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-19 10:32:02 +0200

    node js read file

--- 23.191-Native+Modules/index.js
+++ 23.191-Native+Modules/index.js
@@ -4,3 +4,8 @@ fs.writeFile("message2.txt", "Here is my Message from Node JS", (err) => {
   if (err) throw err;
   console.log("The file has been saved");
 });
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
