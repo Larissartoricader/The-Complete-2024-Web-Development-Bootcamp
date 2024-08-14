2e16cca36bfdc7361cdce98d68827b836196d2d2
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-14 10:10:06 +0200

    solution

--- 23.193-QR-Code-Project/index.js
+++ 23.193-QR-Code-Project/index.js
@@ -5,6 +5,17 @@
 */
 
 import { input } from "@inquirer/prompts";
+import qr from "qr-image";
+import fs from "fs";
+
 const userWebsite = await input({ message: "Enter the URL" });
 
 console.log("here is " + userWebsite);
+
+const qr_image = qr.image(userWebsite, { type: "png" });
+qr_image.pipe(fs.createWriteStream("qr_code.png"));
+
+fs.writeFile("userWebsite.txt", userWebsite, (err) => {
+  if (err) throw err;
+  console.log("The QR Code has been saved");
+});
