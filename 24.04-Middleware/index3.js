064a669ce2a77e292afd56192507924dcfe1b094
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-15 15:02:17 +0200

    add createad middleware logger function

--- 24.04-Middleware/index3.js
+++ 24.04-Middleware/index3.js
@@ -1,7 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+function logger(req, res, next) {
+  console.log("url " + req.url);
+  console.log("Method " + req.method);
+  console.log("IP " + req.ip);
+  console.log("Hostname " + req.hostname);
+  console.log("Protocol " + req.protocol);
+  console.log("Body " + req.body);
+  console.log("Query " + req.query);
+  console.log("Headers " + req.headers);
+  console.log("Path " + req.path);
+  console.log("Response Time " + req.responseTime);
+  console.log("Cookies " + req.cookies);
+  console.log("Referrer " + req.referrer);
+  next();
+}
 
 app.use(logger);
 
