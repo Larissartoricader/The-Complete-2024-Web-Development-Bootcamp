f6370acdf1cbb9eab6d74e4ef95c0a2e014786da
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-19 13:20:34 +0200

    another easy solution

--- 25-EJS/index.js
+++ 25-EJS/index.js
@@ -1,16 +1,40 @@
+// import express from "express";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import ejs from "ejs";
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// const app = express();
+// const port = 3000;
+
+// app.set("views", __dirname + "/views");
+// app.set("view engine", "ejs");
+
+// app.use(express.urlencoded({ extended: true }));
+
+// function typeOfDay() {
+//   const today = new Date();
+//   const numberOfToday = today.getDay();
+//   if (numberOfToday === 0 || numberOfToday === 6) {
+//     return "it's Weekend. Let's celebrate";
+//   } else {
+//     return "It's weekday. Time to work hard";
+//   }
+// }
+
+// app.get("/", (req, res) => {
+//   res.render("index", { typeOfDay: typeOfDay() });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import ejs from "ejs";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
-
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: true }));
 
 function typeOfDay() {
   const today = new Date();
@@ -23,7 +47,7 @@ function typeOfDay() {
 }
 
 app.get("/", (req, res) => {
-  res.render("index", { typeOfDay: typeOfDay() });
+  res.render("index.ejs", { typeOfDay: typeOfDay() });
 });
 
 app.listen(port, () => {
