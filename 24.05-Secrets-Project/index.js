f4df0cc111799bd8e17f03f0e83d8672ea443b3a
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-19 10:50:05 +0200

    bodyparser is now part of express

--- 24.05-Secrets-Project/index.js
+++ 24.05-Secrets-Project/index.js
@@ -5,12 +5,12 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
