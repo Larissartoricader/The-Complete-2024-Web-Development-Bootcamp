509827793404e51154e643a51e162172c19ecb03
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-20 11:06:56 +0200

    solution

--- 25.2-Passing-Data/index.js
+++ 25.2-Passing-Data/index.js
@@ -1,14 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { name } from "ejs";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const numberOfLetters = firstName.length + lastName.length;
+  res.render("index.ejs", {
+    name: `${firstName}  ${lastName}`,
+    number: numberOfLetters,
+  });
 
-app.post("/submit", (req, res) => {});
+  console.log(`${firstName} ${lastName}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
